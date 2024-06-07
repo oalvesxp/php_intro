c059c6e3529799f8f5ccb8366b64c5d32da9fded
OsvaldoNeto <osvaldo.neto@signus.ind.br>
2024-06-07 08:20:04 -0300

    Aula 2.5

--- screen-match.php
+++ screen-match.php
@@ -6,14 +6,15 @@ $nomeFilme = "Top Gun - Maverick";
 $anoLancamento = 2022;
 
 $somaDeNotas = 9;
-$somaDeNotas = 6;
-$somaDeNotas = 8;
-$somaDeNotas = 7.5;
-$somaDeNotas = 5;
+$somaDeNotas += 6;
+$somaDeNotas += 8;
+$somaDeNotas += 7.5;
+$somaDeNotas += 5;
 
 $notaFilme = $somaDeNotas / 5;
 $planoPrime = true;
 
 $incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
 
-echo $nomeFilme;
+echo "Nome do Filme: {$nomeFilme}" . PHP_EOL;
+echo "Nota do Filme: {$notaFilme}" . PHP_EOL;
