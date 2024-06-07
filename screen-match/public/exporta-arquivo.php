8d0e5d5ee6211c83fb2ce29f7ee1b12e9a3df0a7
OsvaldoNeto <osvaldo.neto@signus.ind.br>
2024-06-07 14:14:39 -0300

    Aula 5.2

---
+++ screen-match/public/exporta-arquivo.php
@@ -0,0 +1,10 @@
+<?php
+
+$filme = [
+    'nome' => $_POST['nome'],
+    'lancamento' => $_POST['ano'],
+    'avaliacao' => $_POST['nota'],
+    'genero' => $_POST['genero'],
+];
+
+file_put_contents(__DIR__ . '/filme.json', json_encode($filme));
