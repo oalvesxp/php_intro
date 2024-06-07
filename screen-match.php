ee9ba3ac9b9e99cd037b5ca162cc694c4cb2335a
OsvaldoNeto <osvaldo.neto@signus.ind.br>
2024-06-07 08:42:20 -0300

    Aula 3.3

--- screen-match.php
+++ screen-match.php
@@ -2,7 +2,10 @@
 
 echo "Bem-vindo(a) ao screen match!" . PHP_EOL;
 
-$nomeFilme = "Top Gun - Maverick";
+$nomeFilme = "Top Gun: Maverick";
+$nomeFilme = "Thor: Ragnarok";
+$nomeFilme = "Se beber não case";
+
 $anoLancamento = $argv[1] ?? 2022;
 
 $somaDeNotas = 9;
@@ -21,9 +24,18 @@ echo "Nota do Filme: {$notaFilme}" . PHP_EOL;
 echo "Ano de Lançamento: {$anoLancamento}" . PHP_EOL;
 
 if ($anoLancamento > 2022) {
-    echo "Este filme é um lançamento!";
+    echo "Este filme é um lançamento!" . PHP_EOL;
 } elseif ($anoLancamento > 2020 && $anoLancamento <= 2022) {
-    echo "Esse filme ainda é novo.";
+    echo "Esse filme ainda é novo." . PHP_EOL;
 } else {
-    echo "Esse filme não é um lançamento.";
+    echo "Esse filme não é um lançamento." . PHP_EOL;
 }
+
+$genero = match($nomeFilme) {
+    "Top Gun: Maverick" => 'Ação',
+    "Thor: Ragnarok" => 'Super-Herói',
+    "Se beber não case" => 'Comédia',
+    Default => 'Gênero desconhecido',
+};
+
+echo "Gênero do Filme: {$genero}";
